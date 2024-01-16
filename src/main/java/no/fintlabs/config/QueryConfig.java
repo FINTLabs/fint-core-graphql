597aca28a46bc6a4f2ae705a9418b9b16e9f9737
Hknots <Hknots99@gmail.com>
2024-01-16 12:13:29 +0100

    generate fintMainObjects in query with arguments

--- src/main/java/no/fintlabs/config/QueryConfig.java
+++ src/main/java/no/fintlabs/config/QueryConfig.java
@@ -1,24 +1,55 @@
 package no.fintlabs.config;
 
 import graphql.Scalars;
+import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.reflection.ReflectionService;
+import no.fintlabs.reflection.model.FintMainObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class QueryConfig {
 
+    private final ReflectionService reflectionService;
+
     @Bean
     public GraphQLObjectType buildQuery() {
         return GraphQLObjectType.newObject()
                 .name("Query")
-                .field(GraphQLFieldDefinition.newFieldDefinition().name("test").type(Scalars.GraphQLString).build())
+                .fields(getFieldDefinitions())
                 .build();
+    }
+
+    private List<GraphQLFieldDefinition> getFieldDefinitions() {
+        return reflectionService.getFintMainObjects().values().stream()
+                .map(this::buildFieldDefinition)
+                .collect(Collectors.toList());
+    }
+
+    private GraphQLFieldDefinition buildFieldDefinition(FintMainObject fintMainObject) {
+        return GraphQLFieldDefinition.newFieldDefinition()
+                .name(fintMainObject.getName())
+                .arguments(buildArguments(fintMainObject))
+                .type(Scalars.GraphQLString)
+                .build();
+    }
+
+    private List<GraphQLArgument> buildArguments(FintMainObject fintMainObject) {
+        return fintMainObject.getIdentificatorFields().stream()
+                .map(field -> GraphQLArgument.newArgument()
+                        .name(field)
+                        .type(Scalars.GraphQLString)
+                        .build())
+                .collect(Collectors.toList());
     }
 
 }
