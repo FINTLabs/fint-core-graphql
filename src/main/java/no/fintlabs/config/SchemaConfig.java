846e0b2938c9b15957653fe213f46c2c37250ab9
Hknots <Hknots99@gmail.com>
2024-01-16 11:52:31 +0100

    add schema config

---
+++ src/main/java/no/fintlabs/config/SchemaConfig.java
@@ -0,0 +1,22 @@
+package no.fintlabs.config;
+
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SchemaConfig {
+
+    @Bean
+    public GraphQL graphQL(GraphQLSchema graphQLSchema) {
+        return GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    @Bean
+    public GraphQLSchema graphQLSchema() {
+        return GraphQLSchema.newSchema()
+                .build();
+    }
+
+}
