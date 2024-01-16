01ab9f1949aacf8f224fbdd0b6fbfb9f8a2b3412
Hknots <Hknots99@gmail.com>
2024-01-16 09:50:21 +0100

    initial commit

---
+++ src/main/java/no/fintlabs/FintCoreGraphqlApplication.java
@@ -0,0 +1,13 @@
+package no.fintlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintCoreGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintCoreGraphqlApplication.class, args);
+    }
+
+}
