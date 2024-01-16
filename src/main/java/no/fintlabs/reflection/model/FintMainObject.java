40df33e050d47f6fe7943910120ae79e5a1cb7e1
Hknots <Hknots99@gmail.com>
2024-01-16 10:49:52 +0100

    add getter & set field to final

--- src/main/java/no/fintlabs/reflection/model/FintMainObject.java
+++ src/main/java/no/fintlabs/reflection/model/FintMainObject.java
@@ -1,14 +1,17 @@
 package no.fintlabs.reflection.model;
 
+import lombok.Data;
+import lombok.Getter;
 import no.fint.model.felles.kompleksedatatyper.Identifikator;
 
 import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
 public class FintMainObject extends FintObject {
 
-    private Set<String> identificatorFields;
+    private final Set<String> identificatorFields;
 
     public FintMainObject(Class<?> clazz) {
         super(clazz);
