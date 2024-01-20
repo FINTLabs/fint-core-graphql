b98718a41875b89e7f8cec4f8cfae4eefce3a436
Hknots <Hknots99@gmail.com>
2024-01-20 23:20:55 +0100

    add referenceService

---
+++ src/main/java/no/fintlabs/ReferenceService.java
@@ -0,0 +1,32 @@
+package no.fintlabs;
+
+import graphql.schema.GraphQLObjectType;
+import no.fintlabs.reflection.model.FintObject;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ReferenceService {
+
+    private final Map<Integer, FintObject> fintObjectReferences = new HashMap<>();
+    private final Map<Integer, GraphQLObjectType> objectTypeReferences = new HashMap<>();
+
+    public FintObject getFintObject(int hashCode) {
+        return fintObjectReferences.get(hashCode);
+    }
+
+    public GraphQLObjectType getObjectType(int hashCode) {
+        return objectTypeReferences.get(hashCode);
+    }
+
+    public void addFintObject(int hashCode, FintObject fintObject) {
+        fintObjectReferences.put(hashCode, fintObject);
+    }
+
+    public void addObjectType(int hashCode, GraphQLObjectType objectType) {
+        objectTypeReferences.put(hashCode, objectType);
+    }
+
+}
