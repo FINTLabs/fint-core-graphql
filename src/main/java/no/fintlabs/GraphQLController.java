ba417ce69bc1559354bf3432a427432ed8584045
Hknots <hknots99@gmail.com>
2024-01-29 20:22:20 +0100

    reformat structure of methods

--- src/main/java/no/fintlabs/GraphQLController.java
+++ src/main/java/no/fintlabs/GraphQLController.java
@@ -3,17 +3,15 @@ package no.fintlabs;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import graphql.execution.ExecutionId;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 
 import java.util.Map;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -22,17 +20,19 @@ public class GraphQLController {
     private final GraphQL graphQL;
 
     @PostMapping("/graphql")
-    public ResponseEntity<Object> executeOperation(@RequestBody Map<String, Object> request,
-                                                   ServerWebExchange serverWebExchange) {
-        String query = request.get("query").toString();
+    public ResponseEntity<Object> executeOperation(@RequestBody Map<String, Object> request, ServerWebExchange serverWebExchange) {
+        return ResponseEntity.ok().body(getExecutionResult(request, serverWebExchange).toSpecification());
+    }
 
-        ExecutionInput build = ExecutionInput.newExecutionInput()
+    private ExecutionResult getExecutionResult(Map<String, Object> request, ServerWebExchange serverWebExchange) {
+        return graphQL.execute(getExecutionInput(request.get("query").toString(), serverWebExchange));
+    }
+
+    private ExecutionInput getExecutionInput(String query, ServerWebExchange serverWebExchange) {
+        return ExecutionInput.newExecutionInput()
                 .query(query)
                 .graphQLContext(getGraphQLContext(serverWebExchange))
                 .build();
-
-        ExecutionResult result = graphQL.execute(build);
-        return new ResponseEntity<>(result.toSpecification(), HttpStatus.OK);
     }
 
     private Map<?, Object> getGraphQLContext(ServerWebExchange serverWebExchange) {
