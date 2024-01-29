33f27f267d6e5cf59208670f57fbe94b6bcff57c
Hknots <hknots99@gmail.com>
2024-01-29 20:27:00 +0100

    move restclient config to RequestService

--- src/main/java/no/fintlabs/service/RequestService.java
+++ src/main/java/no/fintlabs/service/RequestService.java
@@ -2,11 +2,9 @@ package no.fintlabs.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
@@ -15,9 +13,13 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @Slf4j
 public class RequestService {
 
-    private final RestClient restClient;
+    @Value("${fint.graphql.base-url:https://play-with-fint.felleskomponent.no}")
+    private String baseUrl;
+
+    private final RestClient restClient = RestClient.builder().baseUrl(baseUrl).build();
 
     public Object getResource(String uri, String authorizationValue) {
+        log.debug("Requesting url: {}{}", baseUrl, uri);
         return restClient.get()
                 .uri(uri)
                 .header(AUTHORIZATION, authorizationValue)
