94013e60a506a25883dc57c3504ec9be0075bc6f
Hknots <hknots99@gmail.com>
2024-02-05 21:07:30 +0100

    add CorePrincipal

--- src/main/java/no/fintlabs/GraphQLController.java
+++ src/main/java/no/fintlabs/GraphQLController.java
@@ -5,7 +5,9 @@ import graphql.ExecutionResult;
 import graphql.GraphQL;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.core.resource.server.security.authentication.CorePrincipal;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,25 +24,27 @@ public class GraphQLController {
     private final GraphQL graphQL;
 
     @PostMapping
-    public ResponseEntity<Object> executeOperation(@RequestBody Map<String, Object> request,
+    public ResponseEntity<Object> executeOperation(@AuthenticationPrincipal CorePrincipal corePrincipal,
+                                                   @RequestBody Map<String, Object> request,
                                                    ServerWebExchange serverWebExchange) {
-        return ResponseEntity.ok().body(getExecutionResult(request, serverWebExchange).toSpecification());
+        return ResponseEntity.ok().body(getExecutionResult(request, serverWebExchange, corePrincipal).toSpecification());
     }
 
-    private ExecutionResult getExecutionResult(Map<String, Object> request, ServerWebExchange serverWebExchange) {
-        return graphQL.execute(getExecutionInput(request.get("query").toString(), serverWebExchange));
+    private ExecutionResult getExecutionResult(Map<String, Object> request, ServerWebExchange serverWebExchange, CorePrincipal corePrincipal) {
+        return graphQL.execute(getExecutionInput(request.get("query").toString(), serverWebExchange, corePrincipal));
     }
 
-    private ExecutionInput getExecutionInput(String query, ServerWebExchange serverWebExchange) {
+    private ExecutionInput getExecutionInput(String query, ServerWebExchange serverWebExchange, CorePrincipal corePrincipal) {
         return ExecutionInput.newExecutionInput()
                 .query(query)
-                .graphQLContext(getGraphQLContext(serverWebExchange))
+                .graphQLContext(getGraphQLContext(serverWebExchange, corePrincipal))
                 .build();
     }
 
-    private Map<?, Object> getGraphQLContext(ServerWebExchange serverWebExchange) {
+    private Map<?, Object> getGraphQLContext(ServerWebExchange serverWebExchange, CorePrincipal corePrincipal) {
         return Map.of(
-                ServerWebExchange.class, serverWebExchange
+                ServerWebExchange.class, serverWebExchange,
+                CorePrincipal.class, corePrincipal
         );
     }
 
